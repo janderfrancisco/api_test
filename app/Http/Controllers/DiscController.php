f166d9b868d2a50a599467e62e3dccf31968bfbb
Jander Oliveira <janderfrancisco@hotmail.com>
2023-07-31 17:06:05 -0300

    base files

---
+++ app/Http/Controllers/DiscController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Disc;
+use App\Http\Requests\StoreDiscRequest;
+use App\Http\Requests\UpdateDiscRequest;
+
+class DiscController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreDiscRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreDiscRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Disc  $disc
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Disc $disc)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Disc  $disc
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Disc $disc)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateDiscRequest  $request
+     * @param  \App\Models\Disc  $disc
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateDiscRequest $request, Disc $disc)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Disc  $disc
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Disc $disc)
+    {
+        //
+    }
+}
